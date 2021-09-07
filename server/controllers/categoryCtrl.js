6d99e1c63c6a1a79d8b39301e27ac204c3ee2b69
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-09-07 17:28:59 -0400

    Succesfully created elements API for categories

--- server/controllers/categoryCtrl.js
+++ server/controllers/categoryCtrl.js
@@ -4,8 +4,54 @@ const Category = require("../models/categoryModel");
 const categoryCtrl = {
   getCategories: async (req, res) => {
     try {
+      // Obtain data from category
       const categories = await Category.find();
+      // Send response of category
       res.json(categories);
+    } catch (err) {
+      // Cacth errors from the response
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  createCategory: async (req, res) => {
+    try {
+      // if user have role = 1 ---> admin
+      // only admin can create , delete and update category
+      const { name } = req.body;
+      // From name of category find element
+      const category = await Category.findOne({ name });
+      // If category exists then send error as it is created
+      if (category) {
+        return res.status(400).json({ msg: "This category already exists." });
+      }
+      // Create a new category of elements
+      const newCategory = new Category({ name });
+      // Wait for the category to be saved
+      await newCategory.save();
+      // And send a response message for category
+      res.json({ msg: "Created a category" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  deleteCategory: async (req, res) => {
+    try {
+      // Get data from category, find the element to delete
+      await Category.findByIdAndDelete(req.params.id);
+      // Send message confirmation of deleted element
+      res.json({ msg: "Deleleted a Category successful" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  updateCategory: async (req, res) => {
+    try {
+      //  Get the new name of element in request body
+      const { name } = req.body;
+      // And update category with the given id
+      await Category.findOneAndUpdate({ _id: req.params.id }, { name });
+      // Send response to elements
+      res.json({ msg: "Updated a category" });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -3,7 +3,15 @@ const router = require("express").Router();
 // Public Libraries
 const categoryCtrl = require("../controllers/categoryCtrl");
 const auth = require("../middleware/auth");
+const authAdmin = require("../middleware/authAdmin");
 // Router actions - GET, POST, DELETE, PUT content
-router.get("/category", categoryCtrl.getCategories); // Get category elements
+router
+  .route("/category")
+  .get(categoryCtrl.getCategories)
+  .post(auth, authAdmin, categoryCtrl.createCategory); // Get and create category elements
+router
+  .route("/category/:id")
+  .delete(auth, authAdmin, categoryCtrl.deleteCategory)
+  .put(auth, authAdmin, categoryCtrl.updateCategory); // delete and update category element
 // Export module of elements
 module.exports = router;
