0774aab426ba5b2f42bd2c7c0e9ee5972ed30e20
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-09-19 14:45:42 -0400

    Successfully inserted link routes to the function - Completed Header section

--- client/src/components/mainPages/Pages.js
+++ client/src/components/mainPages/Pages.js
@@ -1,5 +1,23 @@
+// Public Libraries
 import React from "react";
-
+import { Switch, Route } from "react-router-dom";
+// Private Libraries
+import Products from "./products/Products";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import Cart from "./cart/Cart";
+import NotFound from "./utils/notfound/NotFound";
+// Pages elements
 export default function Pages() {
-  return <div>Main Pages Components</div>;
+  return (
+    <Switch>
+      {/* Page elements  */}
+      <Route path="/" exact component={Products} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
+      <Route path="/cart" exact component={Cart} />
+      {/* Not Found */}
+      <Route path="*" exact component={NotFound} />
+    </Switch>
+  );
 }
