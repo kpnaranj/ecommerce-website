c5e9ed4a7a877e21c3e94ee75ba9e61a8bd4c7a8
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-09-19 13:18:07 -0400

    Successfully cleaned up elements for client site - Working on header elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+// Client imports 
+import React from "react";
+// Main function App
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Hello Client</div>;
 }
-
+// Export app to index.js
 export default App;
