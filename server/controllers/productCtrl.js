67a0e72963cab3d85e968d9a50f8c4af5a089191
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-09-19 13:06:51 -0400

    Successfully created filtering, sorting and paginating of website

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -1,12 +1,74 @@
 // MongoDB models for products
 const Products = require("../models/productModel");
+// Filter, sorting and paginating of products
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+  filtering() {
+    // Get all query elements
+    const queryObj = { ...this.queryString };
+    // Limit elements from page, sort and limit
+    const excludedFields = ["page", "sort", "limit"];
+    // Take excluded field elements and eliminate sections
+    excludedFields.forEach((el) => delete queryObj[el]);
+    // After deleting page
+    /*  console.log({ after: queryObj }); */
+    // Send query string to elements
+    let queryStr = JSON.stringify(queryObj);
+    // Use regex to create expression of elements
+    /* gte = greater than expression, lte=lower than expression 
+      gt = greater than, lt=lower than 
+      regex=complete words 
+    */
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|regex)\b/g,
+      (match) => "$" + match
+    );
+    // Get element from query find
+    this.query.find(JSON.parse(queryStr));
+    // Return elements
+    return this;
+  }
+  sorting() {
+    if (this.queryString.sort) {
+      // We need to know the name that we are sorting
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      // Once we have the name search only that element
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+    return this;
+  }
+  paginating() {
+    // Get the page limits for this query, # of elements
+    const page = this.queryString.page * 1 || 1;
+    // Range of elements to be displayed
+    const limit = this.queryString.limit * 1 || 9;
+    const skip = (page - 1) * 1;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
 // Controls of products
 const productCtrl = {
   getProducts: async (req, res) => {
     try {
+      // Obtain feature of elements
+      const features = new APIfeatures(Products.find(), req.query)
+        .filtering()
+        .sorting()
+        .paginating();
       // Only return list of products available
-      const products = await Products.find();
-      res.json(products);
+      const products = await features.query;
+      res.json({
+        status: "success",
+        result: products.length,
+        products: products,
+      });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
